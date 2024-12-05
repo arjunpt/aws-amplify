568933c232b6556c4252d825c4af29a2d35e31e1
Arjun PT <52270351+arjunpt@users.noreply.github.com>
2024-12-05 11:31:04 +0530

    Delete freeze.py

--- freeze.py
+++
@@ -1,8 +0,0 @@
-from flask_frozen import Freezer
-from app import app  # Ensure this matches the name of your Flask app file (app.py)
-
-freezer = Freezer(app)
-
-if __name__ == "__main__":
-    freezer.freeze()
-
